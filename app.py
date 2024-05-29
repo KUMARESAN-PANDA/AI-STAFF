2a0edda9c541c99b1ab49a7c23013d742d7fe463
KUMARESAN-PANDA <164299143+KUMARESAN-PANDA@users.noreply.github.com>
2024-05-29 09:38:56 +0000

    added extra files

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from flask import Flask ,render_template
-app=Flask(__name__)
+from flask import Flask, render_template,jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+    {
+        'ID': 1,
+        'Title': 'Data Analyst',
+        'Location': 'Bengaluru, India',
+        'Salary': 'Rs. 10,00,000'
+    },
+    {
+        'ID': 2,
+        'Title': 'Data Scintest',
+        'Location': 'Bengaluru, India',
+        'Salary': 'Rs. 20,00,000'
+    },
+    {
+        'ID': 3,
+        'Title': 'Back-end Developer',
+        'Location': 'Bengaluru, India',
+      
+    },
+]
+
 
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
-if __name__=="__main__":
-  app.run(host="0.0.0.0",debug=True)
+@app.route("/api/jobs")
+def job_list():
+  return jsonify(JOBS)
+
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
